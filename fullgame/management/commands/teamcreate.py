9c54f8ecaa639f064a4fce64a67034bee7593c11
Shan Abdulkarim <120406701+ShanAbdulkarim@users.noreply.github.com>
2025-08-19 10:19:20 -0400

    Update teamcreate.py

--- fullgame/management/commands/teamcreate.py
+++ fullgame/management/commands/teamcreate.py
@@ -8,26 +8,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         
-        team_to_create = [{"name": "Arsenal", "rating":85, "formation":4231},
-                          {"name": "Aston Villa", "rating":85, "formation":442},
-                          {"name": "Brighton", "rating":85, "formation":4231},
-                          {"name": "Bournemouth", "rating":85, "formation":4231},
-                          {"name": "Brentford", "rating":85, "formation":532},
-                          {"name": "Chelsea", "rating":85, "formation":4231},
-                          {"name": "Crystal Palace", "rating":85, "formation":523},
-                          {"name": "Everton", "rating":85, "formation":4411},
-                          {"name": "Fulham", "rating":85, "formation":4231},
-                          {"name": "Ipswich Town", "rating":85, "formation":4231},
-                          {"name": "Leicester City", "rating":85, "formation":4231},
-                          {"name": "Liverpool", "rating":85, "formation":4231},
-                          {"name": "Manchester City", "rating":85, "formation":433},
-                          {"name": "Manchester United", "rating":85, "formation":523},
-                          {"name": "Newcastle United", "rating":85, "formation":433},
-                          {"name": "Nottingham Forest", "rating":85, "formation":4231},
-                          {"name": "Southampton", "rating":85, "formation":541},
-                          {"name": "Tottenham Hotspur", "rating":85, "formation":4231},
-                          {"name": "West Ham United", "rating":85, "formation":4231},
-                          {"name": "Wolverhampton Wanderers", "rating":85, "formation":532},
+        team_to_create = [{"name": "Arsenal", "rating":85},
+                          {"name": "Aston Villa", "rating":85},
+                          {"name": "Bournemouth", "rating":85},
+                          {"name": "Brentford", "rating":85},
+                          {"name": "Brighton", "rating":85},
+                          {"name": "Chelsea", "rating":85},
+                          {"name": "Crystal Palace", "rating":85},
+                          {"name": "Everton", "rating":85},
+                          {"name": "Fulham", "rating":85},
+                          {"name": "Ipswich Town", "rating":85},
+                          {"name": "Leicester City", "rating":85},
+                          {"name": "Liverpool", "rating":85},
+                          {"name": "Manchester City", "rating":85},
+                          {"name": "Manchester United", "rating":85},
+                          {"name": "Newcastle", "rating":85},
+                          {"name": "Nottingham Forest", "rating":85},
+                          {"name": "Southampton", "rating":85},
+                          {"name": "Tottenham", "rating":85},
+                          {"name": "West Ham", "rating":85},
+                          {"name": "Wolverhampton Wanderers", "rating":85}
         ]
         # Bulk create the players in the database
         for team_data  in team_to_create:
@@ -37,4 +37,5 @@ class Command(BaseCommand):
         
 
         
+
         self.stdout.write(self.style.SUCCESS('Successfully created players'))
